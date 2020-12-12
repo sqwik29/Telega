b7e5429435ae685e55f9e2a571299c2af8c94b9b
sqwik <z.molodoy@bk.ru>
2020-12-12 19:35:00 +0300

    mi delili predlozhenie

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import telebot
 bot = telebot.TeleBot('1435977321:AAEooPYb77BHDWqWHyRJD3jN5eoFe25Q7jg')
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
-    #bot.send_message(message.from_user.id, "Hello, how can i help you?")
-    if message.json["text"] == "Hello":
-        bot.send_message(message.from_user.id, "Hello")
+    print(message)
+    ist = message.json["text"].split()
+    for a in ist:
+        bot.send_message(message.from_user.id, a)
+
 bot.polling(none_stop=True, interval=0)
